f533c0ac027bbe27ac35da72a3350e531e678aca
cjwit <chris.witulski@gmail.com>
2016-07-16 16:11:07 -0400

    begin home page

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -13,10 +13,56 @@ module.exports = React.createClass({
     },
 
     render: function() {
-        return (
-            <div>
-				Hello World!
-            </div>
+		return (
+			<div>
+				<div className="jumbotron">
+					<div className="container">
+						<h1>Vote!</h1>
+						<p>Easily create, share, and vote in polls</p>
+						<p><a className="btn btn-primary btn-lg" href="#" role="button">Login to get started</a></p>
+					</div>
+				</div>
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-8">
+							Hello World!
+						</div>
+						<div className="col-sm-4">
+							<h2>Recent Polls</h2>
+							<div className="mini-poll">
+								<div className="mini-results"></div>
+								<div className="mini-option"><p>Option 1</p></div>
+								<div className="mini-option"><p>Option 2</p></div>
+								<div className="mini-option"><p>Option 3</p></div>
+							</div>
+							<div className="mini-poll">
+								<div className="mini-results"></div>
+								<div className="mini-option"><p>Option 1</p></div>
+								<div className="mini-option"><p>Option 2</p></div>
+								<div className="mini-option"><p>Option 3</p></div>
+							</div>
+							<div className="mini-poll">
+								<div className="mini-results"></div>
+								<div className="mini-option"><p>Option 1</p></div>
+								<div className="mini-option"><p>Option 2</p></div>
+								<div className="mini-option"><p>Option 3</p></div>
+							</div>
+							<div className="mini-poll">
+								<div className="mini-results"></div>
+								<div className="mini-option"><p>Option 1</p></div>
+								<div className="mini-option"><p>Option 2</p></div>
+								<div className="mini-option"><p>Option 3</p></div>
+							</div>
+							<div className="mini-poll">
+								<div className="mini-results"></div>
+								<div className="mini-option"><p>Option 1</p></div>
+								<div className="mini-option"><p>Option 2</p></div>
+								<div className="mini-option"><p>Option 3</p></div>
+							</div>
+						</div>
+					</div>
+	            </div>
+			</div>
         )
     }
 });
